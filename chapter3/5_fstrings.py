8af951fb1220573d1f2d7500b1bd91202c3e7ab2
Wasiq Ashfaq <wasiqashfaq17@gmail.com>
2025-07-11 18:30:39 +0500

    added the 5 fstring method

---
+++ chapter3/5_fstrings.py
@@ -0,0 +1,8 @@
+# if we want to print a variable in between a string we use the 
+# fstring for that 
+# like we want to print a varable "name"  in betwween 
+# good afternoon  "name"  how are u 
+# we will write it like 
+
+name=input("Enter your Name ")
+print(f"Good afternoon {name}\nWellcome to the company")
